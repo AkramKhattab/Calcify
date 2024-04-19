c87c6b4f92ea20826831ae28e3a5be1fb96d199e
AkramKhattab <137511158+AkramKhattab@users.noreply.github.com>
2024-04-19 21:52:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const outputScreen = document.getElementById("output-screen");
+
+const display = num => outputScreen.value += num;
+// Developed by Akram Khattab
+const calculate = () => {
+    try {
+        outputScreen.value = eval(outputScreen.value);
+    } catch (err) {
+        alert("Invalid equation!");
+    }
+};
+
+const allClear = () => outputScreen.value = "";
+
+const del = () => outputScreen.value = outputScreen.value.slice(0, -1);
